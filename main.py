e739c8b0ba0016181e2b974a50f94edff06ab294
midtown-feet-tickler <vergilspardarreal@gmail.com>
2025-06-10 13:51:52 +0300

    by the nine im still tweaking

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from random import *
+from time import *
 
 pg.init()
 disp = pg.display.set_mode((800, 480))
@@ -23,19 +24,19 @@ apple_y = 120
 score = 0
 snake = [[x, y]]
 while not game_over:
-    clock.tick(score + 1)
+    clock.tick(5)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             game_over = True
             print("game over you racial slur")
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_a:
+            if event.key == pg.K_a and direction != "right":
                 direction = "left"
-            if event.key == pg.K_d:
+            if event.key == pg.K_d and direction != "left":
                 direction = "right"
-            if event.key == pg.K_w:
+            if event.key == pg.K_w and direction != "down":
                 direction = "up"
-            if event.key == pg.K_s:
+            if event.key == pg.K_s and direction != "up":
                 direction = "down"
 
     if direction == "left":
@@ -57,7 +58,20 @@ while not game_over:
         while [apple_x, apple_y] in snake:
             apple_x = randint(1, 19) * 40
             apple_y = randint(1, 11) * 40
-
+    if x >= 820 or x <= -20 or y >= 500 or y <= -20:
+      message = font.render("you lost lmfao", True, WHITE)
+      disp.blit(message, [200, 40])
+      pg.display.update()
+      pg.time.delay(2000)
+      game_over = True
+      break
+    if len(snake) >  4  and snake[-1] in snake[:-1]:
+      message = font.render("you lost lmfao", True, WHITE)
+      disp.blit(message, [200, 40])
+      pg.display.update()
+      pg.time.delay(2000)
+      game_over = True
+      break
     disp.fill(BLACK)
     for i in range(len(snake)):
         pg.draw.rect(disp, GREEN, (snake[i][0], snake[i][1], 40, 40))
