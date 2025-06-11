6b69bf71f48351b1e5c07fb37cd629592e63ebe8
midtown-feet-tickler <vergilspardarreal@gmail.com>
2025-06-11 14:06:38 +0300

    you aint ready for these streets cuh

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import pygame as pg
 from random import *
 from time import *
 
+def check_end_game(x, y, snake):
+    if x >= 800 or x <= -20 or y >= 480 or y <= -20:
+        return True
+    elif len(snake) >  4  and snake[-1] in snake[:-1]:
+        return True
+    else:
+        return False
+
+
+def set_default_values():
+    return 200, 320, 400, 120, "right", 0, [[200, 320]]
+
 pg.init()
 disp = pg.display.set_mode((800, 480))
 pg.display.set_caption("my big green snake")
@@ -17,10 +29,17 @@ RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+apple_img = pg.image.load('appl.png')
+body_img = pg.image.load('body.png')
+snek_img = pg.image.load('snek.png')
+anger_img = pg.image.load('angr!!!.png')
+boost_img = pg.image.load('boost.png')
 x = 200
 y = 320
 apple_x = 400
 apple_y = 120
+boost_x = 600
+boost_y = 80
 score = 0
 snake = [[x, y]]
 while not game_over:
@@ -58,25 +77,41 @@ while not game_over:
         while [apple_x, apple_y] in snake:
             apple_x = randint(1, 19) * 40
             apple_y = randint(1, 11) * 40
-    if x >= 820 or x <= -20 or y >= 500 or y <= -20:
-      message = font.render("you lost lmfao", True, WHITE)
-      disp.blit(message, [200, 40])
-      pg.display.update()
-      pg.time.delay(2000)
-      game_over = True
-      break
-    if len(snake) >  4  and snake[-1] in snake[:-1]:
-      message = font.render("you lost lmfao", True, WHITE)
-      disp.blit(message, [200, 40])
-      pg.display.update()
-      pg.time.delay(2000)
-      game_over = True
-      break
+    if x == boost_x and y == boost_y:
+        snake = [snake[0]] + snake
+        snake = [snake[1]] + snake
+        snake = [snake[2]] + snake
+        score += 3
+        while [boost_x, boost_y] == [apple_x, apple_y] or [boost_x, boost_y] in snake:
+            boost_x = randint(1, 19) * 40
+            boost_y = randint(1, 11) * 40
+    if check_end_game(x, y, snake):
+        # game_over = True
+        disp.blit(anger_img, [x - 40, y - 40])
+        pg.display.update()
+        x, y, apple_x, apple_y, direction, score, snake = set_default_values()
+        message = font.render("you lost lmfao", True, WHITE)
+        disp.blit(message, [200, 40])
+        pg.display.update()
+        pg.time.delay(1000)
+        continue
     disp.fill(BLACK)
     for i in range(len(snake)):
-        pg.draw.rect(disp, GREEN, (snake[i][0], snake[i][1], 40, 40))
-    pg.draw.rect(disp, GREEN, [x, y, 40, 40])
-    pg.draw.rect(disp, RED, [apple_x, apple_y, 40, 40])
+       # pg.draw.rect(disp, GREEN, (snake[i][0], snake[i][1], 40, 40))
+        disp.blit(body_img, snake[i])
+
+    if direction == "up":
+        disp.blit(snek_img, [x, y])
+    if direction == "left":
+        disp.blit(pg.transform.rotate(snek_img, 90), [x, y])
+    if direction == "right":
+        disp.blit(pg.transform.rotate(snek_img, -90), [x, y])
+    if direction == "down":
+        disp.blit(pg.transform.rotate(snek_img, -180), [x, y])
+
+    # pg.draw.rect(disp, RED, [apple_x, apple_y, 40, 40])
+    disp.blit(apple_img, [apple_x, apple_y, 40, 40])
+    disp.blit(boost_img, [boost_x, boost_y, 40, 40])
     message = font.render("score:" +  str(score), True, WHITE)
     disp.blit(message, [8, 8])
     pg.display.update()
